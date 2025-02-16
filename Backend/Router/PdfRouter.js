2d4ee8539d6364f0337ecebb317e10efdb611dcb
Abhi-Aku <abhi2003av@gmail.com>
2025-02-16 13:19:32 +0530

    this
    all code delete

--- Backend/Router/PdfRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { PdfPost } = require('../Controller/PdfController');
-const router = express.Router();
-
-router.post('/send', PdfPost); 
-
-module.exports = router;
--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,64 @@
-const express = require('express')
-const bodyparser=require('body-parser')
-const app = express()
-const port =process.env. PORT ||3000
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+app.use("/files", express.static("files"));
+//mongodb connection----------------------------------------------
+const mongoUrl =
+  "mongodb://localhost:27017/FilePdf";
 
-const db =require('./db')
-app.use(bodyparser.json())
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+//multer------------------------------------------------------------
+const multer = require("multer");
 
-const pdfRouter=require('./Router/PdfRouter')
-app.use('/pdf', pdfRouter)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./files");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+require("./pdfDetails");
+const PdfSchema = mongoose.model("PdfDetails");
+const upload = multer({ storage: storage });
+
+app.post("/upload-files", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  const title = req.body.title;
+  const fileName = req.file.filename;
+  try {
+    await PdfSchema.create({ title: title, pdf: fileName });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
+
+app.get("/get-files", async (req, res) => {
+  try {
+    PdfSchema.find({}).then((data) => {
+      res.send({ status: "ok", data: data });
+    });
+  } catch (error) {}
+});
+
+//apis----------------------------------------------------------------
+app.get("/", async (req, res) => {
+  res.send("Success!!!!!!");
+});
+
+app.listen(5000, () => {
+  console.log("Server Started");
+});
